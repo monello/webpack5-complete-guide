3951efbcd207f2d6994b82da667a21f0cd61cd4f
Morné Louw <louw.morne@gmail.com>
2022-03-24 09:17:04 +0200

    11. publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
     entry: './src/index.js',    // Webpack will use this as you application entry point from where to start building up it's depencenct tree
     output: {
         filename: 'bundle.js',  // Here we tell WP what to call out bundled JS file
-        path: path.resolve(__dirname, 'dist') // Here we tell WP where to place the file. WP will create this dir-structure if it does not yet exist. The path MUST be an absolute-path, so we can use the Node package "path", to help generate it
+        path: path.resolve(__dirname, 'dist'), // Here we tell WP where to place the file. WP will create this dir-structure if it does not yet exist. The path MUST be an absolute-path, so we can use the Node package "path", to help generate it
+        publicPath: 'dist/'     // Tells Webpack where to files your static resoursce. You can also specify a CDN here or a your seperate image server etc. It is also used if you implement "Module Federation". Since WP5, if it's simply just your 'dist' dir you can ommit the 'publicPath` setting altogether.
     },
     mode: 'none',   // This will be where you state the environment this config is intended for: 'development', `production` or 'none'
     module: {
