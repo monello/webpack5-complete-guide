fee1eef3802cfaef921225196a7ea6ef5b4f3305
Morné Louw <louw.morne@gmail.com>
2022-03-27 00:39:26 +0200

    Cleaned up the WebPack Config files for Hello World App

--- hello-world/webpack.production.config.js
+++ hello-world/webpack.production.config.js
@@ -4,10 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        'hello-world': './src/hello-world.js',
-        'kiwi': './src/kiwi.js'
-    },
+    entry: './src/hello-world.js',
     output: {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, './dist'),
@@ -24,25 +21,6 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.(png|jpg)$/,
-                type: 'asset',
-                parser: {
-                    dataUrlCondition: {
-                        maxSize: 3 * 1024
-                    }
-                }
-            },
-            {
-                test: /\.txt/,
-                type: 'asset/source'
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader, 'css-loader'
-                ]
-            },
-            {
                 test: /\.scss$/,
                 use: [
                     MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
@@ -54,8 +32,8 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: [ '@babel/env' ],
-                        plugins: [ '@babel/plugin-proposal-class-properties' ]
+                        presets: ['@babel/env'],
+                        plugins: ['@babel/plugin-proposal-class-properties']
                     }
                 }
             },
@@ -74,16 +52,8 @@ module.exports = {
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             filename: 'hello-world.html',
-            chunks: ['hello-world'],
             title: 'Hello world',
             description: 'Hello world',
-            template: 'src/page-template.hbs'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'kiwi.html',
-            chunks: ['kiwi'],
-            title: 'Kiwi',
-            description: 'Kiwi',
             template: 'src/page-template.hbs'
         })
     ]
