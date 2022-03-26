6c93db34bde7e80debc768e273301fecd8b6fad4
Morné Louw <louw.morne@gmail.com>
2022-03-27 01:25:20 +0200

    Configured ModuleFederation in the Hellow World App

--- hello-world/src/server.js
+++ hello-world/src/server.js
@@ -8,7 +8,7 @@ app.get('/', function (req, res) {
     const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
     res.send(contentFromHtmlFile);
 });
-app.use('/static', express.static(path.resolve(__dirname, '../dist')));
+app.use('/', express.static(path.resolve(__dirname, '../dist')));
 
 app.listen(9001, function () {
     console.log('Application is running on http://localhost:9001');
--- hello-world/webpack.production.config.js
+++ hello-world/webpack.production.config.js
@@ -2,13 +2,14 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederarionPlugin } = require('webpack').container;
 
 module.exports = {
     entry: './src/hello-world.js',
     output: {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, './dist'),
-        publicPath: '/static/'
+        publicPath: 'http://localhost:9001'  // Here we need to tell the Module Federation on which public URL the exposes components can be used. This will be added to the "remoteEntry.js" file we set below in the ModuleFederationPlugin
     },
     mode: 'production',
     optimization: {
@@ -55,6 +56,13 @@ module.exports = {
             title: 'Hello world',
             description: 'Hello world',
             template: 'src/page-template.hbs'
+        }),
+        new ModuleFederarionPlugin({
+            name: 'HelloWorldApp',          // Give this application a Name for Module Federation to identify it
+            filename: 'remoteEntry.js',     // During the build process Webpack will generate a file that contains everything this Application container exports - here we pick a name for that file
+            exposes: {                      // In this Object we describe the components that we want to expose to other applications
+                './HelloWorldButton': './src/components/hello-world-button.js'  // Name that other apps can reference and the path inside this app to the component we are exposing
+            }
         })
     ]
 };
