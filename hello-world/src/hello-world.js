a415f2f6b24733f69f2c781cc599af89d42f53e8
Morné Louw <louw.morne@gmail.com>
2022-03-28 09:56:26 +0200

    58. Nester Module Federation part 1 - Setting up the Image Caption App

--- hello-world/src/hello-world.js
+++ hello-world/src/hello-world.js
@@ -6,8 +6,8 @@ heading.render('hello world');
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
 
-if (process.env.NODE_ENV === 'production') {
-    console.log('Production mode');
-} else if (process.env.NODE_ENV === 'development') {
-    console.log('Development mode');
-}
+// if (process.env.NODE_ENV === 'production') {
+//     console.log('Production mode');
+// } else if (process.env.NODE_ENV === 'development') {
+//     console.log('Development mode');
+// }
