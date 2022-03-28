0ad8be0131534879054a513a63e1dedc1f9cda37
Morné Louw <louw.morne@gmail.com>
2022-03-28 06:13:04 +0200

    55. Set up the Webpack Configs for the Dashboard app - Micro Frontend

---
+++ dashboard/webpack.dev.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+
+module.exports = {
+    entry: './src/daskboard.js',        // Set the correct entry point
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: 'http://localhost:9000/'
+    },
+    mode: 'development',
+    devServer: {
+        port: 9000,                     // Update the port
+        static: {
+            directory: path.resolve(__dirname, './dist'),
+        },
+        devMiddleware: {
+            index: 'dashboard.html',    // Set the "index" HTML file for this app
+            writeToDisk: true
+        },
+        historyApiFallback: {
+            index: 'dashboard.html'     // This tells Webpack to always return 'dashboard.html' no matter what URL you enter into the Browser        }
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'dashboard.html',
+            title: 'Dashboard'
+        }),
+        new ModuleFederationPlugin({
+            name: 'App',        // Give this application a Name for Module Federation to identify it
+            remotes: {          // This app will consume the following Applications
+                HelloWorldApp: 'HelloWorldApp@http://localhost:9001/remoteEntry.js',
+                KiwiApp: 'KiwiApp@http://localhost:9002/remoteEntry.js'
+            }
+        })
+    ]
+};
